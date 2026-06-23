1c3d90c3c56a8731e289e3ff86a6a1f094e63fd2
Srikarchaganti-01 <srikarchaganti134@gmail.com>
2026-06-24 00:34:39 +0530

    completed a custom 404 page for the app

---
+++ src/Pages/NotFound.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Navbar, Footer, Error404 } from "../Components";
+
+function NotFound() {
+  return (
+    <>
+      <Navbar />
+      <Error404 />
+      <Footer />
+    </>
+  );
+}
+
+export default NotFound;
