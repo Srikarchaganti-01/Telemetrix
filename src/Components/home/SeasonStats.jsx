868bc0d8a01a06ce6cd3041dffb3b9e3d9fd67c6
Srikarchaganti-01 <srikarchaganti134@gmail.com>
2026-06-24 00:38:56 +0530

    completed season stas graph using cahrt.js

--- src/Components/home/SeasonStats.jsx
+++ src/Components/home/SeasonStats.jsx
@@ -1,7 +1,78 @@
-import React from "react";
+import { useState } from "react";
+
+import OptionSlider from "../selector/OptionSlider";
+import chartData from "../../data/chartData";
+
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Tooltip,
+  Legend,
+} from "chart.js";
+
+import { Bar } from "react-chartjs-2";
+
+ChartJS.register(CategoryScale, LinearScale, BarElement, Tooltip, Legend);
 
 function SeasonStats() {
-  return <h1 className="text-4xl font-bold ">Season Stats component</h1>;
+  const [selected, setSelected] = useState("Drivers");
+  const options = ["Drivers", "Teams"];
+  const currentData = [
+    ...(selected === "Drivers" ? chartData.drivers : chartData.teams),
+  ].sort((a, b) => b.wins - a.wins);
+  const data = {
+    labels: currentData.map((item) => item.name),
+    datasets: [
+      {
+        label: "Wins",
+        data: currentData.map((item) => item.wins),
+        backgroundColor: "#7f1d1d",
+        borderRadius: 10,
+      },
+    ],
+  };
+
+  const optionsChart = {
+    indexAxis: "y",
+
+    responsive: true,
+
+    plugins: {
+      legend: {
+        display: false,
+      },
+    },
+
+    scales: {
+      x: {
+        ticks: {
+          color: "white",
+        },
+      },
+
+      y: {
+        ticks: {
+          color: "white",
+        },
+      },
+    },
+  };
+
+  return (
+    <div className="bg-[#15151e] p-6 rounded-xl max-h-150 mb-30">
+      <div className="flex justify-between items-center mb-6">
+        <h1 className="font-orbitron text-xl">Season Stats</h1>
+        <OptionSlider
+          options={options}
+          selected={selected}
+          setSelected={setSelected}
+        />
+      </div>
+      <Bar data={data} options={optionsChart} />
+    </div>
+  );
 }
 
 export default SeasonStats;
---
+++ src/Data/chartData.js
@@ -0,0 +1,122 @@
+const chartData = {
+  drivers: [
+    {
+      name: "Verstappen",
+      wins: 8,
+    },
+
+    {
+      name: "Norris",
+      wins: 5,
+    },
+
+    {
+      name: "Leclerc",
+      wins: 3,
+    },
+
+    {
+      name: "Hamilton",
+      wins: 2,
+    },
+    {
+      name: "Red Bull",
+      wins: 10,
+    },
+
+    {
+      name: "McLaren",
+      wins: 5,
+    },
+
+    {
+      name: "Ferrari",
+      wins: 3,
+    },
+
+    {
+      name: "Mercedes",
+      wins: 1,
+    },
+    {
+      name: "Haas",
+      wins: 10,
+    },
+
+    {
+      name: "Racing bulls",
+      wins: 5,
+    },
+
+    {
+      name: "Alpine",
+      wins: 3,
+    },
+
+    {
+      name: "Aston martin`",
+      wins: 1,
+    },
+    {
+      name: "Cadillac",
+      wins: 10,
+    },
+
+    {
+      name: "Audi",
+      wins: 5,
+    },
+  ],
+
+  teams: [
+    {
+      name: "Red Bull",
+      wins: 10,
+    },
+
+    {
+      name: "McLaren",
+      wins: 5,
+    },
+
+    {
+      name: "Ferrari",
+      wins: 3,
+    },
+
+    {
+      name: "Mercedes",
+      wins: 1,
+    },
+    {
+      name: "Haas",
+      wins: 10,
+    },
+
+    {
+      name: "Racing bulls",
+      wins: 5,
+    },
+
+    {
+      name: "Alpine",
+      wins: 3,
+    },
+
+    {
+      name: "Aston martin`",
+      wins: 1,
+    },
+    {
+      name: "Cadillac",
+      wins: 10,
+    },
+
+    {
+      name: "Audi",
+      wins: 5,
+    },
+  ],
+};
+
+export default chartData;
