f289524b67dcb2399e14953e5ecbcf95579fae55
Srikarchaganti-01 <srikarchaganti134@gmail.com>
2026-06-24 00:35:30 +0530

    Completed a reusable option slider component

--- src/Components/selector/OptionSlider.jsx
+++ src/Components/selector/OptionSlider.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
-function OptionSlider() {
-  return <h1 className="text-4xl font-bold ">Option Slider component</h1>;
+function OptionSlider({ options, selected, setSelected }) {
+  return (
+    <div className="flex bg-zinc-800 rounded-lg p-1 w-fit gap-1 ">
+      {options.map((option) => (
+        <button
+          key={option}
+          onClick={() => setSelected(option)}
+          className={`px-4 py-2 rounded-md transition-all duration-300 
+            ${selected === option ? "bg-red-950" : "bg-transparent hover:bg-[#030112]"}`}
+        >
+          {option}
+        </button>
+      ))}
+    </div>
+  );
 }
 
 export default OptionSlider;
