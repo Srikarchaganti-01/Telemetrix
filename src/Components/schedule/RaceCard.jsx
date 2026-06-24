6de946fd36a6d6eec41558f8cd0d76007e50d969
Srikarchaganti-01 <srikarchaganti134@gmail.com>
2026-06-24 18:42:11 +0530

    completed schedule page

--- src/Components/schedule/RaceCard.jsx
+++ src/Components/schedule/RaceCard.jsx
@@ -1,7 +1,42 @@
 import React from "react";
+function RaceCard({ race }) {
+  const state = race.status;
+  return (
+    <div className="w-full h-70  rounded-2xl overflow-hidden flex shadow-lg hover:scale-[1.02] transition-all duration-300  items-center justify-center">
+      <div className="w-[55%] shadow-2xl  h-[80%]">
+        <img
+          src={race.image}
+          alt={race.name}
+          className={`${race.status ? "grayscale opacity-40" : ""} w-full h-full object-cover rounded-3xl`}
+        />
+      </div>
+      <div className="flex-1 p-5 flex flex-col justify-between h-full">
+        <div className="flex flex-col justify-between items-start">
+          <div className="flex justify-between w-full">
+            <div>
+              <p className="text-gray-400 text-sm">Round {race.round}</p>
+            </div>
+            <div className="">{race.status}</div>
+          </div>
 
-function RaceCard() {
-  return <h1 className="text-4xl font-bold ">Race Card component</h1>;
+          <h2 className="text-xl font-bold text-white">{race.name}</h2>
+          <p className="text-gray-400 mt-1">{race.circuit}</p>
+          <div className="flex ">
+            <p className="text-sm text-gray-400">{race.from}</p>
+            <p className="text-sm text-gray-400">-</p>
+            <p className="text-sm text-gray-400">{race.to}</p>
+          </div>
+        </div>
+        <div
+          className={`${race.status ? "" : "grayscale opacity-0"} flex flex-col gap-0.5`}
+        >
+          <p className="text-sm text-gray-400">P 1 : {race.p1}</p>
+          <p className="text-sm text-gray-400">P2 : {race.p2}</p>
+          <p className="text-sm text-gray-400">P3 : {race.p3}</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default RaceCard;
--- src/Pages/Schedule.jsx
+++ src/Pages/Schedule.jsx
@@ -1,19 +1,42 @@
 import React from "react";
-import {
-  Navbar,
-  OptionSlider,
-  RaceCard,
-  RaceStatus,
-  Footer,
-} from "../Components";
+import { useState } from "react";
+import { Navbar, OptionSlider, RaceCard, Footer } from "../Components";
+import scheduleData from "../Data/schedule";
+
 function Schedule() {
+  const [selected, setSelected] = useState("All");
+  const options = ["All", "Past", "Upcoming"];
+  console.log(selected);
+  console.log(selected);
   return (
     <>
-      {/* <h1 className="text-5xl font-bold underline ">Schedule page </h1> */}
       <Navbar />
-      <OptionSlider />
-      <RaceCard />
-      <RaceStatus />
+      <div className="w-full px-6 py-4 text-gray">
+        {/* Option Slider */}
+        <div className="flex justify-end">
+          <div className="flex bg-[#1e1e1e] p-1 rounded-full w-fit">
+            {options.map((opt) => (
+              <button
+                key={opt}
+                onClick={() => setSelected(opt)}
+                className={`px-6 py-2 text-sm font-semibold rounded-full transition-all duration-200 ${
+                  selected === opt
+                    ? "bg-red-900 text-white"
+                    : "text-gray-400 hover:text-white"
+                }`}
+              >
+                {opt}
+              </button>
+            ))}
+          </div>
+        </div>
+
+        <div className="mt-6  w-full grid grid-cols-2  gap-5  ">
+          {scheduleData.map((race) => (
+            <RaceCard key={race.id} race={race} />
+          ))}
+        </div>
+      </div>
       <Footer />
     </>
   );
